74adca3f2401ac8075ccb54bff9242f6f2fc325d
unknown <Thyoudom>
2022-12-22 11:51:10 +0700

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Thyoudom',
+            'email' => 'thyoudom49@gmail.com',
+            'password' => Hash::make('Dom@1122')
+        ]);
+    }
+}
